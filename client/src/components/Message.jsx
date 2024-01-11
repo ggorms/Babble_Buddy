f3c35819229210e27ea77b674b482a0d7ead2cac
Garrett Gorman <garrettgorman1@gmail.com>
2024-01-11 17:28:40 -0500

    Set up messaging functionality with front end and database

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,13 +1,14 @@
 import placeholder from "../assets/placeholder.jpg";
+import { format } from "timeago.js";
 
-function Message({ own }) {
+function Message({ message, own }) {
   return (
     <div className={own ? "message own" : "message"}>
       <div className="messageTop">
         <img className="messageImg" src={placeholder} alt="" />
-        <p className="messageText">Hello this is a message</p>
+        <p className="messageText">{message.text}</p>
       </div>
-      <div className="messageBottom">1 hour ago</div>
+      <div className="messageBottom">{format(message.date)}</div>
     </div>
   );
 }
--- client/src/pages/Messenger.jsx
+++ client/src/pages/Messenger.jsx
@@ -1,12 +1,17 @@
 import ChatOnline from "../components/ChatOnline";
 import Conversation from "../components/Conversation";
 import Message from "../components/Message";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { userConversationsThunk } from "../store/conversation";
 import { useDispatch, useSelector } from "react-redux";
+import { conversationMessagesThunk, newMessageThunk } from "../store/message";
 
 function Messenger() {
+  const [currentChat, setCurrentChat] = useState(null);
+  const [newMessage, setNewMessage] = useState("");
   const dispatch = useDispatch();
+  const scrollRef = useRef();
+
   const user = window.sessionStorage.getItem("userInfo")
     ? JSON.parse(window.sessionStorage.getItem("userInfo"))
     : "";
@@ -17,11 +22,37 @@ function Messenger() {
     (state) => state.conversation.userConversations
   );
 
-  console.log("convos:", conversations);
+  // console.log("convos:", conversations);
 
   useEffect(() => {
     dispatch(userConversationsThunk(user.userId));
   }, [user.userId, dispatch]);
+
+  const messages = useSelector((state) => state.message.conversationMessages);
+
+  // console.log("messages", messages);
+  // console.log("currChat", currentChat);
+
+  const handleSubmitMessage = (e) => {
+    e.preventDefault();
+    const message = {
+      text: newMessage,
+      senderId: user.userId,
+      conversationId: currentChat?.id,
+    };
+    dispatch(newMessageThunk(message)).then(() => {
+      dispatch(conversationMessagesThunk(currentChat?.id));
+    });
+    setNewMessage("");
+  };
+
+  useEffect(() => {
+    dispatch(conversationMessagesThunk(currentChat?.id));
+  }, [currentChat]);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   return (
     <div className="messenger">
@@ -29,28 +60,46 @@ function Messenger() {
         <div className="chatMenuWrapper">
           <input placeholder="Search for friends" className="chatMenuInput" />
           {conversations.map((convo) => (
-            <Conversation
-              key={convo.id}
-              conversation={convo}
-              currentUser={user}
-            />
+            <div key={convo.id} onClick={() => setCurrentChat(convo)}>
+              <Conversation conversation={convo} currentUser={user} />
+            </div>
           ))}
         </div>
       </div>
       <div className="chatBox">
         <div className="chatBoxWrapper">
-          <div className="chatBoxTop">
-            <Message />
-            <Message own={true} />
-            <Message />
-          </div>
-          <div className="chatBoxBottom">
-            <textarea
-              className="chatMessageInput"
-              placeholder="write something..."
-            ></textarea>
-            <button className="chatSubmitButton">Send</button>
-          </div>
+          {currentChat ? (
+            <>
+              <div className="chatBoxTop">
+                {messages.map((message) => (
+                  <div key={message.id} ref={scrollRef}>
+                    <Message
+                      message={message}
+                      own={message.senderId === user.userId}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="chatBoxBottom">
+                <textarea
+                  className="chatMessageInput"
+                  placeholder="write something..."
+                  onChange={(e) => setNewMessage(e.target.value)}
+                  value={newMessage}
+                ></textarea>
+                <button
+                  className="chatSubmitButton"
+                  onClick={handleSubmitMessage}
+                >
+                  Send
+                </button>
+              </div>
+            </>
+          ) : (
+            <span className="noConversationText">
+              Open a conversation to start a chat
+            </span>
+          )}
         </div>
       </div>
       <div className="chatOnline">
