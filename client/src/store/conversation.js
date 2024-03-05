675063d046a7343d65dbddb6f74e871061b5c2d8
Garrett Gorman <garrettgorman1@gmail.com>
2024-03-05 18:09:26 -0500

    updated api calls for production

--- client/src/store/conversation.js
+++ client/src/store/conversation.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://babble-buddy.onrender.com";
 
 const NEW_CONVERSATION = "NEW_CONVERSATION";
 const USER_CONVERSATIONS = "USER_CONVERSATIONS";
