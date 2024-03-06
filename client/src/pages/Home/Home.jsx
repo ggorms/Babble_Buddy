7ef19cc108145511b067c8afeab61945984addcf
Garrett Gorman <garrettgorman1@gmail.com>
2024-03-06 13:50:31 -0500

    changed how home image is loaded

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import "./Home.css";
 import Instructions from "../../components/Instructions/Instructions";
 import AboutUs from "../../components/AboutUs/AboutUs";
+import chatImg from "../../assets/chat.jpg";
 
 function Home() {
   return (
     <div>
-      <div className="homeImageContainer"></div>
+      <div
+        className="homeImageContainer"
+        style={{ backgroundImage: `url(${chatImg})` }}
+      ></div>
       <Instructions />
       <AboutUs />
     </div>
